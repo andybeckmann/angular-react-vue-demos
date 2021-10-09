1f341bba571e803308bceb2378919bfbe7f01db9
Andy Beckmann <contact@andybeckmann.com>
2021-10-09 18:38:11 -0400

    Refactor function based component into class based

--- react/src/App.js
+++ react/src/App.js
@@ -1,88 +1,119 @@
-import { useEffect, useState, useRef } from 'react';
+import { React, Component, createRef } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+export default class App extends Component {
 
-function App() {
-
-	const [todos, updateTodos] = useState([]);
-	const itemDescription = useRef();
-
-	useEffect(() => {
-		const storedItems = localStorage.getItem('todos');
-		updateTodos(storedItems ? JSON.parse(storedItems) : [])
-	}, []);
-
-	function isButtonDisabled() {
-		if (itemDescription.current.value = '') {
-			return false
-		} else {
-			return true
+	constructor(props) {
+		super(props);
+		this.state = { 
+			todos: [],
+			input: ''
 		}
+		this.description = createRef();
+		this.add = this.add.bind(this);
+		this.onChange = this.onChange.bind(this);
 	}
 
-	function addItem(event) {
+	add () {
+		this.props.onButtonClick(this.state.value);
+		this.setState({ input: '' })
+	}
+
+	onChange (e) {
+		this.SetState({ input: e.target.value })
+	}
+
+	componentDidMount () {
+		const storedItems = localStorage.getItem('todos');
+		this.setState({
+			todos: storedItems ? JSON.parse(storedItems) : []
+		})
+	}
+
+	componentDidUpdate () {
+		//
+	}
+
+	isButtonDisabled() {
+		//
+	}
+
+	addItem (event) {
 		event.preventDefault();
 		const newItem = {
-			description: itemDescription.current.value,
+			description: this.description.current.value,
 			completed: false
 		};
-		const updatedList = [...todos, newItem];
-		updateTodos(updatedList);
-		localStorage.setItem('todos', JSON.stringify(updatedList));
-		itemDescription.current.value = ''
+		const list = [...this.state.todos, newItem]
+		this.setState({
+			todos: list
+		})
+		localStorage.setItem('todos', JSON.stringify(list));
+		this.setState({
+			input: ''
+		})
+		//this.description.current.value = ''
 	}
 
-	function deleteItem(index) {
-		todos.splice(index, 1);
-		const updatedList = [...todos]
-		updateTodos(updatedList);
-		localStorage.setItem('todos', JSON.stringify(updatedList));
+	deleteItem(index) {
+		this.state.todos.splice(index, 1);
+		const list = [...this.state.todos]
+		this.setState({
+			todos: list
+		})
+		localStorage.setItem('todos', JSON.stringify(list));
 	}
 
-	function toggleItemStatus(index) {
-		todos[index][1] = !todos[index][1]
-		const updatedList = [...todos]
-		updateTodos(updatedList);
-		localStorage.setItem('todos', JSON.stringify(updatedList));
+	toggleItemStatus(index) {
+		const list = [...this.state.todos]
+		list[index][1] = !list[index][1];
+		this.setState({
+			todos: list
+		})
+		localStorage.setItem('todos', JSON.stringify(list));
 	}
 
-	return (
-		<div className="app">
-			<div className="app--main">
-				<img src={logo} className="app--logo" alt="Logo" />
-				<form className="app--main--add-item" onSubmit={addItem}>
-					<label>Add a task</label>
-					<div>
-						<input ref={itemDescription} placeholder="So, what's next?" />
-						<button 
-							disabled={itemDescription === ""}
-						>
-							Add
-						</button>
-					</div>
-				</form>
-				<ul className="app--main--items">
-					{todos.map((item, index) => (
-					<li key={index} className={ item[1] ? "completed" : ''}>
-						<button
-							onClick={() => toggleItemStatus(index)}
+	render() {
+		return (
+			<div className="app">
+				<div className="app--main">
+					<img src={logo} className="app--logo" alt="Logo" />
+					<form className="app--main--add-item" onSubmit={(e) => this.addItem(e)}>
+						<label>Add a task</label>
+						<div>
+							<input 
+								ref={this.description}
+								onChange={e => this.setState({ input: e.target.value })} 
+								value={this.state.input}
+								placeholder="So, what's next?" />
+							<button 
+								disabled={!this.state.input}
 							>
-						</button>
-						<div className="app--main--items-item-text">
-							{item.description}
-						</div> 
-						<button 
-							className="delete"
-							onClick={() => deleteItem(index)}
-						>
-							&times;
-						</button>
-					</li>
-					))}
-				</ul>
+								Add
+							</button>
+						</div>
+					</form>
+					<ul className="app--main--items">
+						{this.state.todos.map((item, index) => (
+						<li key={index} className={ item[1] ? "completed" : ''}>
+							<button
+								onClick={() => this.toggleItemStatus(index)}
+								>
+							</button>
+							<div className="app--main--items-item-text">
+								{item.description}
+							</div> 
+							<button 
+								className="delete"
+								onClick={() => this.deleteItem(index)}
+							>
+								&times;
+							</button>
+						</li>
+						))}
+					</ul>
+				</div>
 			</div>
-		</div>
-	);
+		)
+	}
 }
-
-export default App;
