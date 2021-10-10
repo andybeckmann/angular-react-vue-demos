cec88fd524dfe636acc543e1c4c4dadcfc6fed11
Andy Beckmann <contact@andybeckmann.com>
2021-10-10 19:51:48 -0400

    Add functions

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular';
+	todos: Array<any> = [];
+	input: String = '';
+
+	ngOnInit() {
+		const storedData = localStorage.getItem('todos');
+		this.todos = JSON.parse(storedData as string) || []
+	}
+
+	addItem () {
+		const newItem = {
+			description: this.input,
+			completed: false
+		};
+		this.todos.push(newItem)
+		localStorage.setItem('todos', JSON.stringify(this.todos));
+		this.input = ''
+	}
+
+	deleteItem(index: number) {
+		this.todos.splice(index, 1);
+		localStorage.setItem('todos', JSON.stringify(this.todos));
+	}
+
+	toggleItemStatus(index: number) {
+		this.todos[index][1] = !this.todos[index][1];
+		localStorage.setItem('todos', JSON.stringify(this.todos));
+	}
 }
